962f8c0aaeb154f790e57262137572ad792b4ad0
Larissa Oliveira <worklarissaoliveira@gmail.com>
2024-10-01 13:35:21 -0300

    build: build activities and components

--- Aplicacao/app/src/main/java/com/larissa/challengeodontoprev/MainActivity.kt
+++ Aplicacao/app/src/main/java/com/larissa/challengeodontoprev/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.larissa.challengeodontoprev
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var btnComeco: Button = findViewById(R.id.mainBtn)
+
+        btnComeco.setOnClickListener {
+            val navegarMain = Intent(this, PerguntaDissertativaActivity::class.java)
+            startActivity(navegarMain)
+
+
         }
     }
 }
