08f58f3036d7b76d30e468617f631c4ca9dc95d0
Luna Faustino <lu.faustino2000@gmail.com>
2025-03-19 17:03:26 -0300

    refactor: correções no App.js

--- challenge-odontoprev/App.js
+++ challenge-odontoprev/App.js
@@ -1,7 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
-import{ createNativeStackNavigator } from '@react-navigation/native-stack'
-import { useState } from 'react';
-import { StyleSheet, Text, View, Button, Image, Modal } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { StyleSheet } from 'react-native';
 import Home from './screens/Home';
 import EnviarRaioX from './screens/EnviarRaioX';
 import TelaAnalises from './screens/TelaAnalises';
@@ -12,24 +11,16 @@ const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-            headerShown: false
-          }} >
-          <Stack.Screen name = 'Home' component={Home} />
-          <Stack.Screen name = 'EnviarRaio' component={EnviarRaioX} />
-          <Stack.Screen name = 'TelaAnalises' component={TelaAnalises} />
-          <Stack.Screen name = 'TelaImportar' component={ImportarRaio} />
-          <Stack.Screen name = 'TelaPergunta' component={TelaPergunta} />
-        </Stack.Navigator>
-      </NavigationContainer>
-
-    
-
-
-
-
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="EnviarRaio" component={EnviarRaioX} />
+        <Stack.Screen name="TelaImportar" component={ImportarRaio} />
+        <Stack.Screen name="TelaAnalises" component={TelaAnalises} />
+        <Stack.Screen name="TelaPergunta" component={TelaPergunta} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-
